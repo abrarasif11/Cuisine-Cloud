4505dfd00bd50b0f2cd23f7bcb11dc29746ea02c
abrarasif11 <faasif929@gmail.com>
2025-09-12 18:57:35 +0600

    header and footer complete

--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -1,11 +1,82 @@
-import React from 'react'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
+  const links = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/myApplication">My Application</NavLink>
+      </li>
+      <li>
+        <NavLink to="/createJob">Create Job</NavLink>
+      </li>
+      <li>
+        <NavLink to="/allJobs">All Jobs</NavLink>
+      </li>
+      <li>
+        <NavLink to="/myPostedJobs">My Posted Jobs</NavLink>
+      </li>
+    </>
+  );
   return (
-    <div>
-      Header
-    </div>
-  )
-}
+    <div className="navbar bg-black text-white ">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            {links}
+          </ul>
+        </div>
+        <div className="flex  items-center">
+          {/* <Link
+            to="/"
+            className=" font-josefin font-semibold normal-case text-2xl"
+          >
+            <img className="w-[55px]" src={headerPhoto} alt="" />
+          </Link> */}
+        </div>
 
-export default Header
+        <Link to="/" className="btn btn-ghost text-xl text-white">
+          Cuisine Cloud
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{links}</ul>
+      </div>
+      <div className="">
+        <>
+          <Link className="mr-3 underline text-white hover:text-gray-400" to="/register">
+            Register
+          </Link>
+          <Link to="/signin">
+            <button className="btn bg-white hover:bg-gray-400 text-black">Log In</button>
+          </Link>
+        </>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Router.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <div className="max-w-screen-xl mx-auto">
+      <RouterProvider router={router} />
+    </div>
   </StrictMode>
 );
