0441243ad20c407a306d9ea6257df20dd0618748
abrarasif11 <faasif929@gmail.com>
2025-09-12 20:16:34 +0600

    create category swiper

--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -2,39 +2,50 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import { Pagination } from "swiper/modules";
-import img1 from '../../../assets/home/slide1.jpg'
-import img2 from '../../../assets/home/slide2.jpg'
-import img3 from '../../../assets/home/slide3.jpg'
-import img4 from '../../../assets/home/slide4.jpg'
-import img5 from '../../../assets/home/slide5.jpg'
+import img1 from "../../../assets/home/slide1.jpg";
+import img2 from "../../../assets/home/slide2.jpg";
+import img3 from "../../../assets/home/slide3.jpg";
+import img4 from "../../../assets/home/slide4.jpg";
 const Category = () => {
   return (
-    <Swiper
-      slidesPerView={4}
-      spaceBetween={30}
-      centeredSlides={true}
-      pagination={{
-        clickable: true,
-      }}
-      modules={[Pagination]}
-      className="mySwiper"
-    >
-      <SwiperSlide>
-        <img src={img1} alt="" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={img2} alt="" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={img3} alt="" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={img4} alt="" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={img5} alt="" />
-      </SwiperSlide>
-    </Swiper>
+    <div className="mx-auto">
+      <h2 className="text-4xl font-semibold text-center mb-10">Online Order</h2>
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        centeredSlides={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper mb-20"
+      >
+        <SwiperSlide>
+          <img src={img1} alt="" />
+          <h1 className="text-4xl uppercase text-center -mt-20 pb-5 text-white">
+            Salads
+          </h1>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img2} alt="" />
+          <h1 className="text-4xl uppercase text-center -mt-20 text-white">
+            Pizzas
+          </h1>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img3} alt="" />
+          <h1 className="text-4xl uppercase text-center -mt-20 text-white">
+            Soups
+          </h1>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img4} alt="" />
+          <h1 className="text-4xl uppercase text-center -mt-20 text-white">
+            Deserts
+          </h1>
+        </SwiperSlide>
+      </Swiper>
+    </div>
   );
 };
 
