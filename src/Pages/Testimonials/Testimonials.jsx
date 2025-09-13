2965463b349dcf57c7710c6098f44c907e3132b0
abrarasif11 <faasif929@gmail.com>
2025-09-13 10:38:58 +0600

    react rating use

--- src/Pages/Testimonials/Testimonials.jsx
+++ src/Pages/Testimonials/Testimonials.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import SectionTitle from "../../Components/SectionTitle/SectionTitle";
 import { Swiper, SwiperSlide } from "swiper/react";
-
+import { Rating } from "@smastrom/react-rating";
+import { FaQuoteLeft } from "react-icons/fa";
+import "@smastrom/react-rating/style.css";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
@@ -21,12 +23,16 @@ const Testimonials = () => {
       />
       <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
         {reviews.map((review) => (
-          <SwiperSlide key={review._id}
-          >
-            <div className="mt-24">
-                <p>{review.details}</p>
-                <p className="text-2xl text-orange-400">{review.name}</p>
-                <p>{review.rating}</p>
+          <SwiperSlide key={review._id}>
+            <div className="m-20 flex flex-col items-center">
+              <Rating
+                style={{ maxWidth: 180 }}
+                value={review.rating}
+                readOnly
+              />
+             <FaQuoteLeft className="mt-10" size={100} />
+              <p className="py-8">{review.details}</p>
+              <p className="text-2xl text-orange-400">{review.name}</p>
             </div>
           </SwiperSlide>
         ))}
