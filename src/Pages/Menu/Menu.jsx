3baf7b47be7e81834e88a1862c73db9f63e61a54
abrarasif11 <faasif929@gmail.com>
2025-09-13 12:30:35 +0600

    Using React Parallax

--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
+import Cover from "../../Shared/Cover/Cover";
+import menuImg from '../../assets/menu/banner3.jpg';
+import PopularMenu from "../Home/PopularMenu/PopularMenu";
 const Menu = () => {
   return (
     <div>
       <Helmet>
         <title>Cuisine Cloud | Menu </title>
       </Helmet>
-      <p>Ticket: </p>
+      <Cover img={menuImg} title="Our Menu"/>
+      <PopularMenu/>
+      <Cover img={menuImg} title="Our Menu"/>
+      <PopularMenu/>
+      <Cover img={menuImg} title="Our Menu"/>
+      <PopularMenu/>
+      <Cover img={menuImg} title="Our Menu"/>
+      <PopularMenu/>
     </div>
   );
 };
